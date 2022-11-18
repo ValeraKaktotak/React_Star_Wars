ae992cde93b4a23687bf5f78740fc950d9169c96
Valera <valerakaktotak@gmail.com>
2022-11-18 23:16:58 +0200

    add empty component home page

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,12 +1,14 @@
 import {Route, Routes} from "react-router-dom";
 import PeoplePage from "../PeoplePage";
+import HomePage from "../HomePage";
 import style from './App.module.css'
 
 const App = () => {
     return (
         <div className={style.App}>
             <Routes>
-                <Route path="/" element={<PeoplePage />}/>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/people" element={<PeoplePage />}/>
             </ Routes>
         </div>
     );
