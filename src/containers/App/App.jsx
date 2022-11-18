73d3b3ea99809276dd593d74b11fe544344367ae
Valera <valerakaktotak@gmail.com>
2022-11-18 22:47:10 +0200

    add browser router, routes and route

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,13 @@
+import {Route, Routes} from "react-router-dom";
 import PeoplePage from "../PeoplePage";
 import style from './App.module.css'
 
 const App = () => {
     return (
         <div className={style.App}>
-            <PeoplePage />
+            <Routes>
+                <Route path="/" element={<PeoplePage />}/>
+            </ Routes>
         </div>
     );
 }
