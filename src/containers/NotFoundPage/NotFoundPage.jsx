2d5b5672c6bee5066fbcd7ff5f83a38d58d8d125
Valera <valerakaktotak@gmail.com>
2022-11-19 22:20:40 +0200

    add empty 404_Page component and add route for it

---
+++ src/containers/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import style from './NotFoundPage.module.css'
+
+const NotFoundPage = () => {
+    return(
+        <div className={style.not_found_page}>
+            Not Found
+        </div>
+    )
+}
+
+export default NotFoundPage
