ea652afa93552d4f7e767e2c373c013703bfa7ee
Valera <valerakaktotak@gmail.com>
2022-11-19 11:22:50 +0200

    create routesConfig and include it in app.js

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,14 +1,26 @@
-import {Route, Routes} from "react-router-dom";
+import {NavLink, Route, Routes} from "react-router-dom";
 import PeoplePage from "../PeoplePage";
 import HomePage from "../HomePage";
 import style from './App.module.css'
+import routesConfig from "../../routes/routesConfig";
 
 const App = () => {
     return (
         <div className={style.App}>
+            <div className="header__text">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/people">People</NavLink>
+            </div>
             <Routes>
-                <Route path="/" element={<HomePage />}/>
-                <Route path="/people" element={<PeoplePage />}/>
+                {routesConfig.map((element,index)=>(
+                    <Route
+                        key={index}
+                        path={element.path}
+                        element={element.component()}
+                    />
+                ))}
+                {/*<Route path="/" element={<HomePage />}/>*/}
+                {/*<Route path="/people" element={<PeoplePage />}/>*/}
             </ Routes>
         </div>
     );
