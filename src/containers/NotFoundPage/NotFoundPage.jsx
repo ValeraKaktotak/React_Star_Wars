da7d1a5cfbb36ad3d2a390f2cf45817fbf886ddd
Valera <valerakaktotak@gmail.com>
2022-11-19 23:03:19 +0200

    done not found page

--- src/containers/NotFoundPage/NotFoundPage.jsx
+++ src/containers/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,16 @@
+import {useLocation} from "react-router";
+import img from './images/page404.png'
 import style from './NotFoundPage.module.css'
 
 const NotFoundPage = () => {
+    const currentUrl = useLocation()
+    console.log(currentUrl)
     return(
         <div className={style.not_found_page}>
-            Not Found
+            <img src={img} alt="404"/>
+            <div className={style.not_found_page__text}>
+                No math for <span className={style.not_found_page__yellow}>{currentUrl.pathname}</ span>
+            </div>
         </div>
     )
 }
