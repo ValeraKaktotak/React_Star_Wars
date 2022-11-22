e67971b31b61df3a4b891dc1a1bf3d513950bc57
Valera <valerakaktotak@gmail.com>
2022-11-22 14:04:41 +0200

    create empty component PersonPage

---
+++ src/containers/PersonPage/PersonPage.jsx
@@ -0,0 +1,11 @@
+import style from './PersonPage.module.css'
+
+const PersonPage = () => {
+    return(
+        <div className={style.personPage}>
+            Person
+        </div>
+    )
+}
+
+export  default PersonPage
