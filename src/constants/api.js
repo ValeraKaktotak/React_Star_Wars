2f9ed683908a4333c9832148a369709c21045070
Valera <valerakaktotak@gmail.com>
2022-11-22 17:05:54 +0200

    create useState for person data

--- src/constants/api.js
+++ src/constants/api.js
@@ -4,9 +4,10 @@ export const HTTPS = 'https://'
 
 //SWAPI
 export const SWAPI_ROOT = 'swapi.dev/api/'
-export const SWAPI_PEOPLE = 'people'
+export const SWAPI_PEOPLE = 'people/'
 export const SWAPI_PAGE_NUMBER = '?page='
 export const API_PEOPLE = HTTPS+SWAPI_ROOT+SWAPI_PEOPLE+SWAPI_PAGE_NUMBER
+export const API_PERSON = HTTPS+SWAPI_ROOT+SWAPI_PEOPLE
 
 //visualguide
 const GUIDE_ROOT_IMG = 'https://starwars-visualguide.com/assets/img/'
--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,14 +1,40 @@
-import style from './PersonPage.module.css'
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import PreLoader from "../../components/PreLoader";
+import withErrorApi from "../../hoc-helpers/withErrorApi";
+import {getApiResource} from "../../utils/network";
+import {API_PERSON} from "../../constants/api";
+import style from './PersonPage.module.css'
 
-const PersonPage = () => {
-    let param = useParams()
-    console.log(param)
+const PersonPage = ({setErrorApi}) => {
+    let [person, setPerson] = useState(null)
+    let [isLoading, setIsLoading] = useState(false)
+    let id = useParams().id
+
+    useEffect(()=>{
+        (async()=>{
+            let res = await getApiResource(API_PERSON+id)
+            if(res){
+                setPerson(res)
+                setIsLoading(true)
+                setErrorApi(false)
+            }else{
+                setErrorApi(true)
+            }
+        })()
+    }, [])
+
+
     return(
         <div className={style.personPage}>
-            Person
+            {!isLoading ?
+                <PreLoader/>:
+                <div>
+                    {person.name}
+                </div>
+            }
         </div>
     )
 }
 
-export default PersonPage
+export default withErrorApi(PersonPage)
