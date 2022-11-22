9d6ec9a2c66785927a814a9dba39c9b49ca35104
Valera <valerakaktotak@gmail.com>
2022-11-22 18:27:58 +0200

    person page small fix

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -8,6 +8,7 @@ import style from './PersonPage.module.css'
 
 const PersonPage = ({setErrorApi}) => {
     let [person, setPerson] = useState(null)
+    let [personName, setPersonName] = useState(null)
     let [isLoading, setIsLoading] = useState(false)
     let id = useParams().id
 
@@ -15,7 +16,17 @@ const PersonPage = ({setErrorApi}) => {
         (async()=>{
             let res = await getApiResource(API_PERSON+id)
             if(res){
-                setPerson(res)
+                setPerson([
+                    {title: 'birth_year', data: res.birth_year},
+                    {title: 'eye_color', data: res.eye_color},
+                    {title: 'gender', data: res.gender},
+                    {title: 'hair_color', data: res.hair_color},
+                    {title: 'height', data: res.height},
+                    {title: 'homeworld', data: res.homeworld},
+                    {title: 'mass', data: res.mass},
+                    {title: 'skin_color', data: res.skin_color},
+                ])
+                setPersonName(res.name)
                 setIsLoading(true)
                 setErrorApi(false)
             }else{
@@ -30,7 +41,10 @@ const PersonPage = ({setErrorApi}) => {
             {!isLoading ?
                 <PreLoader/>:
                 <div>
-                    {person.name}
+                    <h1>{personName}</h1>
+                    {person.map(({title, data = 'unknown'})=>{
+                        return <p key={title}>{title}: {data}</p>
+                    })}
                 </div>
             }
         </div>
