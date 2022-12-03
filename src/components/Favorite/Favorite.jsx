7d9fc11b5fa652ac379a6eac359e513d412b787e
Valera <valerakaktotak@gmail.com>
2022-12-03 16:43:48 +0200

    finished component Favorite

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,10 +1,23 @@
 import style from './Favorite.module.css'
 import bookmark from './images/favorite_bookmark.svg'
+import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
 
 const Favorite = () => {
+    let [count, setCount] = useState(null)
+    let favoriteData = useSelector(state => state.favorite)
+
+    useEffect(()=>{
+        let keys = Object.keys(favoriteData).length
+        setCount(keys)
+    }, [favoriteData])
+
     return(
         <div className={style.bookmark__container}>
-            <span className={style.bookmark__counter}>0</span>
+            {count? <div className={style.bookmark__counter}>
+                <span>{count}</span>
+            </div>: null
+            }
             <img className={style.bookmark__img} src={bookmark} alt="bookmark"/>
         </div>
     )
