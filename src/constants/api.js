dee6cd78f2b0ff9cae2483ca6ac71f7cb1a97708
Valera <valerakaktotak@gmail.com>
2022-12-07 20:32:05 +0200

    fix some troubles in api and create search api

--- src/constants/api.js
+++ src/constants/api.js
@@ -5,11 +5,14 @@ export const HTTPS = 'https://'
 //SWAPI
 
 //export const SWAPI_ROOT = 'swapi.dev/api/'
-export const SWAPI_ROOT = 'www.swapi.tech/api/'
+//export const SWAPI_ROOT = 'www.swapi.tech/api/'
+export const SWAPI_ROOT = 'swapi.dev/api/'
 export const SWAPI_PEOPLE = 'people/'
 export const SWAPI_PAGE_NUMBER = '?page='
+export const SWAPI_SEARCH_PARAM = '?search='
 export const API_PEOPLE = HTTPS+SWAPI_ROOT+SWAPI_PEOPLE+SWAPI_PAGE_NUMBER
 export const API_PERSON = HTTPS+SWAPI_ROOT+SWAPI_PEOPLE
+export const API_SEARCH = HTTPS+SWAPI_ROOT+SWAPI_PEOPLE+SWAPI_SEARCH_PARAM
 
 //visualguide
 const GUIDE_ROOT_IMG = 'https://starwars-visualguide.com/assets/img/'
--- src/containers/Searchpage/SearchPage.jsx
+++ src/containers/Searchpage/SearchPage.jsx
@@ -1,11 +1,37 @@
+import {useState} from "react";
+import withErrorApi from "../../hoc-helpers/withErrorApi";
+import {getApiResource} from "../../utils/network";
+import {API_SEARCH, SWAPI_PEOPLE} from "../../constants/api";
 import style from './SearchPage.module.css'
 
-const SearchPage = () => {
+const SearchPage = ({setErrorApi}) => {
+    const [searchValue, setSearchValue] = useState('')
+
+    const getResponse = async (param) => {
+        const searchResponse = await getApiResource(API_SEARCH+param)
+        if(searchResponse){
+            setErrorApi(false)
+            console.log(searchResponse.results)
+        }else{
+            setErrorApi(true)
+        }
+    }
+
+    const handleInputChange = (e) =>{
+        setSearchValue(e.target.value)
+        getResponse(e.target.value)
+    }
     return(
         <>
             <h1 className="header__text">Search Page</h1>
+            <input
+                type="text"
+                value={searchValue}
+                onChange={handleInputChange}
+                placeholder="Input characters names"
+            />
         </>
     )
 }
 
-export default SearchPage
+export default withErrorApi(SearchPage)
