c3702c2a7addee31efc5746016d50e9696672222
Valera <valerakaktotak@gmail.com>
2022-12-07 22:04:41 +0200

    finished search logic

--- src/containers/Searchpage/SearchPage.jsx
+++ src/containers/Searchpage/SearchPage.jsx
@@ -1,17 +1,31 @@
 import {useState} from "react";
 import withErrorApi from "../../hoc-helpers/withErrorApi";
 import {getApiResource} from "../../utils/network";
-import {API_SEARCH, SWAPI_PEOPLE} from "../../constants/api";
+import {API_SEARCH} from "../../constants/api";
 import style from './SearchPage.module.css'
+import {getPeopleId, getPeopleImg} from "../../services/getPeopleData";
 
 const SearchPage = ({setErrorApi}) => {
     const [searchValue, setSearchValue] = useState('')
+    const [people, setPeople] = useState([])
+
 
     const getResponse = async (param) => {
         const searchResponse = await getApiResource(API_SEARCH+param)
         if(searchResponse){
+            const peopleList = searchResponse.results.map(({name, url})=>{
+                const id = getPeopleId(url)
+                const img = getPeopleImg(id)
+                return (
+                    {
+                        id,
+                        name,
+                        img
+                    }
+                )
+            })
+            setPeople(peopleList)
             setErrorApi(false)
-            console.log(searchResponse.results)
         }else{
             setErrorApi(true)
         }
