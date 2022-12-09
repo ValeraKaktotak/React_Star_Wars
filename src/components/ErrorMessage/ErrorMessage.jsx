6147551c644d14533ba23af90ce4a39245ea697a
Valera <valerakaktotak@gmail.com>
2022-12-09 14:47:49 +0200

    finished with video component

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,12 +1,18 @@
+import UiVideo from "../Ui/UiVideo";
+import errorVideo from './video/coverr-wild-horses-6062-1080p.mp4'
 import style from './ErrorMessage.module.css'
 
 const ErrorMessage = () => {
     return(
-        <p className={style.errorMessage__text}>
-            The dark side of the force has won.<br />
-            We cannot display data.<br />
-            Come back when we fix everything
-        </p>
+        <>
+            <p className={style.errorMessage__text}>
+                The dark side of the force has won.<br />
+                We cannot display data.<br />
+                Come back when we fix everything
+            </p>
+            <UiVideo src={errorVideo} classes={style.errorMessage__video}
+            playbackRate={1.0}/>
+        </>
     )
 }
 
--- src/components/Ui/UiVideo/UiVideo.jsx
+++ src/components/Ui/UiVideo/UiVideo.jsx
@@ -1,10 +1,28 @@
+import cn from 'classnames'
+import {useEffect, useRef} from "react";
 import style from './UiVideo.module.css'
 
-const UiVideo = () => {
+
+const UiVideo = ({
+    src,
+    classes,
+    playbackRate = 1.0
+ }) => {
+    const videoRef = useRef()
+
+    useEffect(()=>{
+        videoRef.current.playbackRate = playbackRate;
+    }, [])
+
     return(
-        <div>
-            Video
-        </div>
+        <video
+            ref={videoRef}
+            loop
+            autoPlay
+            muted
+            className={cn(style.video__wrapper ,classes)}>
+            <source src={src} />
+        </video>
     )
 }
 
